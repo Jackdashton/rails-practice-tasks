af81d824f44782286c4fdb66e83981591b97feae
jack ashton <jackdashton@outlook.com>
2023-02-25 23:09:19 +0100

    Finished all 7 CRUD actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,42 @@ class TasksController < ApplicationController
     @task = Task.find(id)
     # individual task, find it using the id variable
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    id = params[:id]
+    # set id variable equal to whatever is in the url id
+    @task = Task.find(id)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # No need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+    # redirects to the object/instance of show page
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+    # see_other responds to HTTP status code - redirection.
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+    # only these 3 params are permitted
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/tasks', to: 'tasks#index'
   # verb+url to controller#method = view page must match index
-  get '/tasks/:id', to: 'tasks#show', as: :task
+  get '/tasks', to: 'tasks#index'
+
+  # We need the #new method to go before show, because otherwise the id would be id=new
+  # this doesn't exist obviously, therefore it causes an error.
+  get '/tasks/new', to: 'tasks#new'
+  # Need a post to submit form, and verb+url = post+tasks isn't taken, therefore use this.
+  post 'tasks', to: 'tasks#create'
+
   # dyanmic reference, with controller method 'show' and html.erb file 'show'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
